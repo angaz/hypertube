28761bf20534d4c0e0e076430f7d741ee0f90189
kgani <kgani@student.42.fr>
2016-11-20 10:17:11 +0200

    fixed routing and added angular router over and above the express router; started adding front end

---
+++ assets/app/components/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'hypertube-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent {
+
+}
---
+++ assets/app/components/series/series.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'hypertube-series',
+  templateUrl: './series.component.html',
+  styleUrls: ['./series.component.css']
+})
+export class SeriesComponent {
+
+}
