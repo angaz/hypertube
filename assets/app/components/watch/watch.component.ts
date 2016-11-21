02084804949b55f756f99012a0ac74f1e75e8b30
Angus Dippenaar <angusdippenaar@gmail.com>
2016-11-21 16:45:39 +0200

    I have no idea what I'm doing

---
+++ assets/app/components/watch/watch.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { YtsService } from '../../services/yts.service';
+
+@Component ({
+  selector: 'hypertube-watch',
+  providers: [YtsService],
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent {
+  isHide = false;
+  selectedMovie: Object = {
+    'title' : "No Title has been set...",
+  };
+  defaultMovies: any;
+
+  constructor(private _ytsService:YtsService){
+    this._ytsService.defaultOutput().subscribe(res => {
+      this.defaultMovies = res.data.movies;
+    });
+  }
+
+  movieProfileClose(){
+    this.isHide = false;
+  }
+
+  movieUpdateInfo(movieObject: any){
+    this.selectedMovie = movieObject;
+  }
+}
