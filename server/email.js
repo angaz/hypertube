bd86fe8671c46f9de0898fa255294fd1512b3ada
avw1987 <arno.vanwyk1987@gmail.com>
2016-11-30 18:29:15 +0200

    validating account

--- server/email.js
+++ server/email.js
@@ -1,7 +1,56 @@
 'use strict';
 
-let sendMail = (email, name, token) => {
+function sendConfirmation(email, name, token) {
 	const SENDGRID_REGISTRATION_TEMPLATE = 'd75099cd-9a9e-4496-a6ba-ecf9797fe054';
+	const SENDGRID_API_KEY = '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';
+	const HOST = 'http://localhost:4200';
+	const sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
+	let sg = require('sendgrid')(SENDGRID_API_KEY);
+
+	let request = sg.emptyRequest({
+		method: 'POST',
+		path: '/v3/mail/send',
+		body: {
+			personalizations: [
+				{
+					to: [
+						{
+							'email': email,
+						},
+					],
+					'substitutions': {
+						'-name-': name,
+						'-activation_code-': HOST + '/activate?email=' + email + '&activation=' + token
+					},
+					subject: 'Welcome to Tubular, ' + name,
+				},
+			],
+			from: {
+				email: 'info@tubular.com',
+			},
+			content: [
+				{
+					type: 'text/html',
+					value: '<p></p>',
+				},
+			],
+			'template_id': SENDGRID_REGISTRATION_TEMPLATE,
+		},
+	});
+
+	return new Promise((resolve, reject) => {
+		sg.API(request, (err, success) => {
+			if (err) {
+				return reject(err);
+			}
+			resolve(success);
+		});
+	});
+}
+
+
+function sendReset(email, name, token) {
+	const SENDGRID_REGISTRATION_TEMPLATE = 'a8a1c62e-5c5c-4574-b3e4-38ccc30401ad';
 	const SENDGRID_API_KEY = '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';
 	const HOST = 'http://localhost:4200';
 	const sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
@@ -22,7 +71,7 @@ let sendMail = (email, name, token) => {
 						'-name-': name,
 						'-activation_code-': HOST + '/activate/?email=' + email + '&activation=' + token
 					},
-					subject: 'Welcome to Tubular, ' + name,
+					subject: 'Tubular password reset, ' + name,
 				},
 			],
 			from: {
@@ -37,14 +86,28 @@ let sendMail = (email, name, token) => {
 			'template_id': SENDGRID_REGISTRATION_TEMPLATE,
 		},
 	});
-	sg.API(request, function (error, response) {
-		if (error) {
-			console.log('Error response received');
-		}
-		console.log(response.statusCode);
-		console.log(response.body);
-		console.log(response.headers);
-	});
-};
 
-module.exports = sendMail;
+	return new Promise((resolve, reject) => {
+		sg.API(request, (err, success) => {
+			if (err) {
+				return reject(err);
+			}
+			resolve(success);
+		});
+	});
+}
+
+function confirmToken(email, name, token) {
+
+}
+
+function resetPassword(email, name, token) {
+
+}
+
+module.exports = {
+	sendConfirmation: sendConfirmation,
+	sendReset: sendReset,
+	confirmToken: confirmToken,
+	resetPassword: resetPassword
+};
--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
 const User = require('./models/user');
+const email = require('./email');
 
 router.post('/', (req, res, next) => {
 	let user = new User({
@@ -13,7 +15,6 @@ router.post('/', (req, res, next) => {
 		username: req.body.username,
 		password: bcrypt.hashSync(req.body.password, 10)
 	});
-	console.log(user);
 	user.save((err, result) => {
 		if (err) {
 			return res.status(500).json({
@@ -21,20 +22,49 @@ router.post('/', (req, res, next) => {
 				error: err
 			});
 		}
-		let token = jwt.sign({user: user}, 'secretllamaissecret', {expiresIn: 21600}); //Expires in 6 hours
-		require('./email')(user.email, user.firstName, token);
-		console.log('sending email');
 		res.status(201).json({
 			message: 'User created successfully',
 			obj: result
 		});
 	});
+	console.log('Sending email');
+	let token = jwt.sign({user: user}, 'secretllamaissecret', {expiresIn: 21600}); //Expires in 6 hours
+	email.sendConfirmation(user.email, user.firstName, token)
+		.then(result => res.json(result))
+		.catch(error => res.status(500).json(error));
 });
 
-router.post('/activate/:email?:token?', (req, res, next) => {
-	console.log("ACTIVIGTIITITOISFGLHJFKLAFKADHFOI");
-	});
+router.post('/resend', (req, res, next) => {
 
+});
+
+router.get('/activate', (req, res, next) => {
+	jwt.verify(req.query.activation, 'secretllamaissecret', (err, decoded) => {
+		if (err) {
+			return res.status(401).json({
+				title: 'Not Authenticated',
+				error: err
+			});
+		}
+		User.findOne({ name: decoded.user.username }, function (err, doc){
+			if(err) {
+				return res.status(500).json({
+					title: 'Username not found',
+					error: err
+				});
+			}
+			User.update({username: decoded.user.username},
+				{ $set : { validated: 1 }}, (err, doc) =>	{
+				if(err) {
+					return res.status(500).json({
+						title: 'Email not validated',
+						error: err
+					});
+				}
+			});
+		});
+	});
+});
 
 router.post('/signin', (req, res, next) => {
 	User.findOne({username: req.body.username}, (err, user) => {
